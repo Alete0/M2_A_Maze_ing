940b0e7092c9dd24c4dd51f7e26041dbb4b3406e
Alejandro Gomez Garcia <alejandr@c1r3s6.42malaga.com>
2026-06-12 11:33:37 +0200

    OUTPUT_FILE: encoder.py created, to generate de hex outputfile of the map, aun hace falta afinar cuando tengamos la solucion, y hace falta conectarlo en el flujo, probado y funciona

---
+++ encoder.py
@@ -0,0 +1,32 @@
+import sys
+from typing import Tuple
+
+Coord = Tuple[int, int]
+Grid = list[list[int]]
+
+
+def line_to_hex(row: list[int]) -> str:
+    return "".join(f"{cell:X}" for cell in row)
+
+
+def gen_maze_file(
+        output_path: str,
+        maze: Grid,
+        entry_c: Coord,
+        exit_c: Coord,
+        path_solution: str = ""
+) -> None:
+    try:
+        with open(output_path, 'w') as f:
+            for row in maze:
+                f.write(line_to_hex(row) + "\n")
+            f.write("\n")
+
+            f.write(f"{entry_c[0]},{entry_c[1]}\n")
+            f.write(f"{exit_c[0]},{exit_c[1]}\n")
+
+            f.write(f"{path_solution}\n")
+
+    except IOError as e:
+        print(f"Critical error writing to output file: {e}", file=sys.stderr)
+        sys.exit(1)
